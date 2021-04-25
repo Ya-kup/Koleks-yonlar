74ba92cc0cc924c3ef1fe41849cfb8b7ebd1ab83
Ya-kup <eemyakupkurt@gmail.com>
2021-04-26 00:19:23 +0300

    Add project files.

---
+++ Koleksıyonlar/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Koleksıyonlar
+{
+    class MyList<T>
+    {
+        T[] elemanlar;
+        public MyList()
+        {
+            elemanlar = new T[0];
+        }
+
+        public void Add(T eleman)
+        {
+            T[] tempArray = elemanlar;
+            elemanlar = new T[elemanlar.Length + 1];
+            for (int i = 0; i < tempArray.Length  ; i++)
+            {
+                elemanlar[i] = tempArray[i];
+            }
+            elemanlar[elemanlar.Length - 1] = eleman;
+        }
+
+    }
+}
---
+++ Koleksıyonlar/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Koleksıyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> elemanlar = new MyList<string>();
+            elemanlar.Add("yakup");
+          
+        }
+    }
+}
